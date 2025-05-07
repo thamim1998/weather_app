9367d2397c280477d7c101315bf6e4609ee13c13
thamim1998 <thamim1997@gmail.com>
2025-05-07 15:50:00 +0200

    types and weather class completed

---
+++ src/api/weather.ts
@@ -0,0 +1,51 @@
+import { API_CONFIG } from "./config";
+import { Coordinates, ForecastData, GeocodingData, WeatherData } from "./types";
+
+class WeatherAPI {
+  private createUrl(endpoint: string, params: Record<string, string | number>) {
+    const searchParams = new URLSearchParams({
+      appid: API_CONFIG.API_KEY,
+      ...params,
+    });
+
+    return `${endpoint}?${searchParams.toString()} `;
+  }
+
+  //Dont know what is the type of the fetch data, So using <T>
+  private async fetchData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Weather API Error: ${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  async getCurrentWeather({ lat, lon }: Coordinates): Promise<WeatherData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/weather`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+    return this.fetchData<WeatherData>(url);
+  }
+
+  async getForcast({ lat, lon }: Coordinates): Promise<ForecastData> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+      units: API_CONFIG.DEFAULT_PARAMS.units,
+    });
+    return this.fetchData<ForecastData>(url);
+  }
+
+  async reverGeocode({ lat, lon }: Coordinates): Promise<GeocodingData[]> {
+    const url = this.createUrl(`${API_CONFIG.BASE_URL}/reverse`, {
+      lat: lat.toString(),
+      lon: lon.toString(),
+     limit:1,
+    });
+    return this.fetchData<GeocodingData[]>(url);
+  }
+}
+
+export const weatherAPI = new WeatherAPI();
